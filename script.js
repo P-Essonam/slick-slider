efc612460a5519bd280cc45a625aac6bf6b22983
P_essonam <essonammaximin@gmail.com>
2023-11-26 12:31:59 +0100

    slider with slick

---
+++ script.js
@@ -0,0 +1,10 @@
+
+$(document).ready(function(){
+    $('.slick-carousel').slick({
+        arrows: true,
+        centerMode: true,
+        centerPadding: '0px',
+        slidesToShow: 3,
+        dots: true,
+    });
+  });
